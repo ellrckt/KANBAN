6191d4bae2044fc72dcdf4ef4b6f91ddd2501a4f
Daniil Zubrik <daniilzubrik@gmail.com>
2025-02-07 12:38:31 +0300

    Test

--- backend/testApp/main.py
+++ backend/testApp/main.py
@@ -7,14 +7,9 @@ import bcrypt
 from routers.user import router as user_router
 from routers.user_registration import router as user_registration_router
 from routers.login import router as login_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
-# @asynccontextmanager
-# async def lifespan(application: FastAPI):
-
-#     yield
-
-#     await db_helper.dispose()
 
 
 app = FastAPI(
@@ -22,11 +17,20 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(user_registration_router)
 app.include_router(login_router)
 
-
-@app.get("/get_users/")
-def get_users():
-    print("hello world")
